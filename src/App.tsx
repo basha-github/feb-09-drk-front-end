c472e25aedafb1a03d47b21bb7a0ee18b3d19b28
basha-github <sharifabee.alur@gmail.com>
2026-02-09 10:19:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import QuestionsPage from './components/QuestionsPage'
+
+export default function App() {
+  return (
+    <div>
+
+
+<QuestionsPage />
+
+
+    </div>
+  )
+}
---
+++ src/components/QuestionsPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function QuestionsPage() {
+  return (
+    <div>
+
+
+1. Who invented Java Programming?
+<br></br>
+a) Guido van Rossum
+<br></br>
+b) James Gosling
+<br></br>
+c) Dennis Ritchie
+<br></br>
+d) Bjarne Stroustrup
+
+
+    </div>
+  )
+}
