f468d5cbb0d887a440538d9404602ef1e02844d4
basha-github <sharifabee.alur@gmail.com>
2026-02-09 10:30:11 +0530

    v2

--- src/components/QuestionsPage.tsx
+++ src/components/QuestionsPage.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 
+import '../css/question.css'
+
 export default function QuestionsPage() {
   return (
-    <div>
+    <div className='questiomMargin'>
+
 
 
 1. Who invented Java Programming?
 <br></br>
-a) Guido van Rossum
 <br></br>
+ <input type="checkbox" />&nbsp;&nbsp;&nbsp;
+  a) Guido van Rossum
+<br></br>
+<br></br>
+ <input type="checkbox" />&nbsp;&nbsp;&nbsp;
+
 b) James Gosling
 <br></br>
+<br></br>
+ <input type="checkbox" />&nbsp;&nbsp;&nbsp;
+
 c) Dennis Ritchie
 <br></br>
+<br></br>
+ <input type="checkbox" />&nbsp;&nbsp;&nbsp;
 d) Bjarne Stroustrup
 
 
